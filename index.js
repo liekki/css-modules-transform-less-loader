76b0da7c062c9b700b70cdad406a37ccd1c522ca
Matti Vakkilainen <matti.vakkilainen@yle.fi>
2017-03-29 14:21:35 +0300

    add code

---
+++ index.js
@@ -0,0 +1,15 @@
+const less = require('less')
+const path = require('path')
+
+module.exports = (data, file) => {
+  less.render(data, {
+    filename: path.resolve(file)
+  }, function(error, output) {
+    if(!error) {
+      console.log(file + ': ', output.css.toString('utf8'))
+      return output.css.toString('utf8')
+    } else {
+      console.log('ERROR: ', error)
+    }
+  })
+};
