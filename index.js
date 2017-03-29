b80a4f5442f2bee402b53578cbd2385390e8bf6c
Matti Vakkilainen <matti@vakkilainen.org>
2017-03-29 16:07:46 +0300

    wrap the whole thing in try catch

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const less = require('less')
 const path = require('path')
 
 module.exports = (data, file) => {
-  less.render(data, {
-    filename: path.resolve(file)
-  }, function(error, output) {
-    if(!error) {
-      console.log(file + ': ', output.css.toString('utf8'))
-      return output.css.toString('utf8')
-    } else {
-      console.log('ERROR: ', error)
-    }
-  })
+  try {
+    less.render(data, {
+      filename: path.resolve(file)
+    }, function(error, output) {
+      if(!error) {
+        console.log(file + ': ', output.css.toString('utf8'))
+        return output.css.toString('utf8')
+      } else {
+        console.log('ERROR: ', error)
+      }
+    })
+  } catch (e) {
+    console.error(e);
+  }
 };
